366c623af92b8e19696f8509a26649c56d9736cc
Martin Algañaraz <idcmardelplata@gmail.com>
2022-07-27 22:52:01 -0300

    Add packer as plugin manager and tokyonight as theme

---
+++ lua/user/plugins.lua
@@ -0,0 +1,16 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Use dependency and run lua function after load
+  use {
+    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+    config = function() require('gitsigns').setup() end
+  }
+  use 'ghifarit53/tokyonight-vim'
+end)
