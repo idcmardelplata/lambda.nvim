ab220162d316c5e9cbe77f6e954950240bb0cd6f
Martin Algañaraz <idcmardelplata@gmail.com>
2022-07-28 23:28:28 -0300

    Added html and elixirls lsp providers

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "rust_analyzer", "denols", "tsserver" }
+local servers = { "jsonls", "sumneko_lua", "rust_analyzer", "denols", "tsserver", "elixirls", "html" }
 
 lsp_installer.setup({
 	ensure_installed = servers,
