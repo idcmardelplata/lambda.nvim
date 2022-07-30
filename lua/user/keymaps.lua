64b0d35913087a7c83c8f7dd79928f4ab8b0dbc9
Martin Algañaraz <idcmardelplata@gmail.com>
2022-07-30 16:40:18 -0300

    refactor add keybinding for write the current buffer

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -63,6 +63,8 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 keymap("n","<leader>f","<cmd>lua require('telescope.builtin').find_files( require('telescope.themes').get_dropdown({previewer = false}))<cr>", opts)
 keymap("n", "<C-t>", "<cmd>Telescope live_grep<cr>", opts)
 
+keymap("n", "<C-s>", "<cmd>:write<cr>", opts)
+
 -- Neovim configurations
 
 -- Terminal --
