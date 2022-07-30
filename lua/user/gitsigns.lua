1ed98d8fa6e48a83a491ee730d27c3ddf20bbca0
Martin Algañaraz <idcmardelplata@gmail.com>
2022-07-30 16:48:00 -0300

    chore: delete some innecesary keymaps for gitsigns

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -74,13 +74,13 @@ gitsigns.setup {
     map('n', '<leader>hR', gs.reset_buffer)
     map('n', '<leader>hp', gs.preview_hunk)
     map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    -- map('n', '<leader>tb', gs.toggle_current_line_blame)
     map('n', '<leader>hd', gs.diffthis)
     map('n', '<leader>hD', function() gs.diffthis('~') end)
-    map('n', '<leader>td', gs.toggle_deleted)
+    -- map('n', '<leader>td', gs.toggle_deleted)
 
     -- Text object
-    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    -- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end
 }
 
