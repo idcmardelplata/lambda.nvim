fdf8546ff261987a50bb19355105786c2bd3f428
Martin Algañaraz <idcmardelplata@gmail.com>
2022-07-31 00:57:06 -0300

    very basic feline configuration

--- lua/user/feline.lua
+++ lua/user/feline.lua
@@ -5,3 +5,34 @@ if not is_ok then
 end
 
 fe.setup()
+
+local components = {
+  active = {},
+  inactive = {}
+}
+
+table.insert(components.active, {})
+table.insert(components.active, {})
+table.insert(components.active, {})
+
+
+table.insert(components.inactive, {})
+table.insert(components.inactive, {})
+
+-- Prepara la insercion de componentes
+table.insert(components.active[1], {
+  provider = 'git_branch'
+})
+
+table.insert(components.active[1], {
+  provider = 'git_diff_addedd'
+
+})
+
+table.insert(components.active[2], { provider = 'diagnostic_errors' })
+
+table.insert(components.active[3], { provider = 'diagnostic_warnings' })
+
+table.insert(components.inactive[1], {  provider = 'diagnostic_hints' })
+
+table.insert(components.inactive[2], {  provider = 'diagnostic_info' })
