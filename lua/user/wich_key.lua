b5e82056facac94a52ef2b87d164bf47ee87f14c
Martin Algañaraz <idcmardelplata@gmail.com>
2022-08-01 15:58:25 -0300

    feat: add git actions to which-key menu

--- lua/user/wich_key.lua
+++ lua/user/wich_key.lua
@@ -89,6 +89,35 @@ local files = {
   e = {"<cmd>:NvimTreeToggle<cr>", "NvimTree"}
 }
 
+local gs = require("gitsigns")
+
+ local git = {
+   name = "Git",
+   n = {function()
+       if vim.wo.diff then return ']c' end
+       vim.schedule(function() gs.next_hunk() end)
+       return '<Ignore>'
+     end, "Go to next hunk",
+   },
+   p = {
+     function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+     end, "Go to previous hunk",
+   },
+   s = { "<cmd>Gitsigns stage_hunk<cr>", "Stage hunk" },
+   r = { "<cmd>Gitsigns reset_hunk<cr>", "reset hunk" },
+   b = { "<cmd>Gitsigns stage_buffer<cr>", "stage buffer" },
+   u = {gs.undo_stage_hunk, "undo stage junk"},
+   R = {gs.reset_buffer, "Reset buffer"},
+   P = {gs.preview_hunk, "Preview hunk"},
+   B = {function() gs.blame_line({full = true}) end, "Blame line"},
+   d = {gs.diffthis, "Show diff in the file"},
+ }
+ -- local lsp = {}
+-- local db = {}
 key.register(files, { prefix = "f"})
+key.register(git, { prefix = "g"})
 
 
