39a20079ed745ff22ad3bbe6f8b0f7c445779b29
Martin Algañaraz <idcmardelplata@gmail.com>
2022-09-11 22:47:39 -0300

    fix: fix update of lspsaga

--- lua/user/lsp/saga.lua
+++ lua/user/lsp/saga.lua
@@ -23,9 +23,9 @@ move_in_saga = { prev = '<C-p>',next = '<C-n>'},
 -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
 diagnostic_header =  { " ", " ", " ", "ﴞ " },
 -- show diagnostic source
-show_diagnostic_source = true,
+-- show_diagnostic_source = true,
 -- add bracket or something with diagnostic source, just have 2 elements
-diagnostic_source_bracket = {},
+-- diagnostic_source_bracket = {},
 -- preview lines of lsp_finder and definition preview
 max_preview_lines = 10,
 -- use emoji lightbulb in default
@@ -64,7 +64,7 @@ code_action_keys = {
 },
 rename_action_quit = "<C-c>",
 rename_in_select = true,
-definition_preview_icon = "  ",
+-- definition_preview_icon = "  ",
 -- show symbols in winbar must nightly
 symbol_in_winbar = {
     in_custom = false,
