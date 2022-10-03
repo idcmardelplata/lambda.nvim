c6f823ca04854ab390034b41364520229b2cd3df
Martin Algañaraz <idcmardelplata@gmail.com>
2022-10-02 23:31:25 -0300

    fix: update null-ls to most recent commit

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -77,7 +77,7 @@ return packer.startup(function(use)
   --Lsp Stuff
   use({ "neovim/nvim-lspconfig", commit = "148c99bd09b44cf3605151a06869f6b4d4c24455" }) -- enable LSP
 	use({ "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" }) -- simple to use language server installer
-	-- use({ "jose-elias-alvarez/null-ls.nvim", commit = "ff40739e5be6581899b43385997e39eecdbf9465" }) -- for formatters and linters
+	use({ "jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" }) -- for formatters and linters
 
   -- Telescope
   use({ "nvim-telescope/telescope.nvim", commit = "d96eaa914aab6cfc4adccb34af421bdd496468b0" })
