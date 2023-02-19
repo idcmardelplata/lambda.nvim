acc840d1132127433e148ef03628b19a4e9bff77
Martin Algañaraz <idcmardelplata@gmail.com>
2023-02-19 18:42:40 -0300

    chore (plugin) neorg conceal configuration is done

--- lua/user/plugins/neorg.lua
+++ lua/user/plugins/neorg.lua
@@ -1,17 +1,45 @@
 return {
   "nvim-neorg/neorg",
+  tag = "v2.0.1",
   dependencies = { { "nvim-lua/plenary.nvim" } },
   build = ":Neorg sync-parsers",
   opts = {
     load = {
       ["core.defaults"] = {}, -- Loads default behaviour
-      ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.autocommands"] = {}, 
+      ["core.integrations.treesitter"] = {},
+      ["core.norg.concealer"] = {
+        config = {
+          folds = true,
+          icon_presset = "varied",
+          markup_preset = "brave",
+          icons = {
+            definition = { 
+              enabled = { true },
+              multi_prefix = {
+                enabled  = true,
+                icon = "⋙ ",
+                query = "(multi_definition_prefix) @icon",
+              },
+              multi_suffix = {
+                enabled = true,
+                icon = "⋘ ",
+                query = "(multi_definition_suffix) @icon"
+              },
+              single = {
+                enabled = true,
+                icon = "≡",
+                query = "[ (single_definition_prefix) (link_target_definition) @no-conceal ] @icon",
+              }
+            },
+          }
+        }
+      }, -- Adds pretty icons to your documents
       ["core.norg.completion"] = {
         config = {
           engine = "nvim-cmp",
         }
       },
-      ["core.integrations.treesitter"] = {},
       ["core.norg.dirman"] = { -- Manages Neorg workspaces
       config = {
         workspaces = {
