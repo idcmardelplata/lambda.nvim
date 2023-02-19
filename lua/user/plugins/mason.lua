6e867b48031b7cb9814d5ab0c614a020840cd8bc
Martin Algañaraz <idcmardelplata@gmail.com>
2023-02-19 18:41:29 -0300

    fix: replace sumneko_lua for lua_ls provider

--- lua/user/plugins/mason.lua
+++ lua/user/plugins/mason.lua
@@ -9,7 +9,7 @@ return {
     local mason_config = require("mason-lspconfig")
 
     local servers = {
-      "sumneko_lua",
+      "lua_ls",
       "rust_analyzer",
       "denols",
       "tsserver",
@@ -38,33 +38,18 @@ return {
       ensure_installed = servers,
     }
 
-    require("lspconfig").sumneko_lua.setup {
-      settings = {
-        Lua = {
-          runtime = {
-            version = "Lua 5.3",
-            path = {
-              '?.lua',
-              '?/init.lua',
-              vim.fn.expand'~/.luarocks/share/lua/5.3/?.lua',
-              vim.fn.expand'~/.luarocks/share/lua/5.3/?/init.lua',
-              '/usr/share/5.3/?.lua',
-              '/usr/share/lua/5.3/?/init.lua'
-            }
-          },
-          workspace = {
-            library = {
-              vim.fn.expand'~/.luarocks/share/lua/5.3',
-              '/usr/share/lua/5.3'
-            }
-          }
-        }
-      }
-    }
 
     require("lspconfig").emmet_ls.setup {}
 
     local group = vim.api.nvim_create_augroup("MasonLspGroup", {clear = true})
+
+    vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+      group = group,
+      pattern = {"*.lua"},
+      callback = function ()
+        require("lspconfig").lua_ls.setup {}
+      end
+    })
 
     vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
       group = group,
