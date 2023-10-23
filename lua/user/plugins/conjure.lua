9572cf0b2f1a9e97cb55c8685bd5a67fcfda385e
Martin Algañaraz <idcmardelplata@gmail.com>
2023-10-23 15:55:01 -0300

    chore: now conjure load the server on start

--- lua/user/plugins/conjure.lua
+++ lua/user/plugins/conjure.lua
@@ -1,37 +1,19 @@
 return {
     "Olical/conjure",
     ft = { "lisp",  "clojure", "fennel", "python" }, -- etc
-    -- [Optional] cmp-conjure for cmp
-    -- dependencies = {
-    --     {
-            -- "PaterJason/cmp-conjure",
-            -- config = function()
-            --     local cmp = require("cmp")
-            --     local config = cmp.get_config()
-            --     table.insert(config.sources, {
-            --         name = "buffer",
-            --         option = {
-            --             sources = {
-            --                 { name = "conjure" },
-            --             },
-            --         },
-            --     })
-            --     cmp.setup(config)
-            -- require "cmp".setup {
-            --   sources = {
-            --     name = "conjure"
-            --   }
-            -- }
-            --  end
-    --     },
-    -- },
     dependencies = {"PaterJason/cmp-conjure",   "monkoose/parsley" , "gpanders/nvim-parinfer" },
     config = function(_, opts)
         require "cmp".setup {
           source = { name = "conjure"},
         }
+
+        -- auto start the server
+        vim.fn.jobstart("ros run --eval '(ql:quickload :swank)'  --eval '(swank:create-server :dont-close t)'")
+
+
         require("conjure.main").main()
         require("conjure.mapping")["on-filetype"]()
+
     end,
     init = function()
 	       -- Set configuration options here
