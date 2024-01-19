ba91a6d2b156a4083948ed2b48cdfe1b95d5bc95
Martin Algañaraz <idcmardelplata@gmail.com>
2024-01-19 19:43:55 -0300

    latest config

--- lua/user/plugins/neotest_python.lua
+++ lua/user/plugins/neotest_python.lua
@@ -1,18 +1,20 @@
 return {
   "nvim-neotest/neotest-python",
-  dependencies={"nvim-neotest/neotest", "nvim-lua/plenary.nvim" },
+  dependencies={"nvim-neotest/neotest", "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
   lazy = true,
   ft="python",
   config = function()
     require("neotest").setup({
       adapters = {
         require("neotest-python")({
-          dap = {
-            justMyCode = false,
-            console = "integratedTerminal",
-          },
+          -- dap = {
+          --   justMyCode = false,
+          --   console = "integratedTerminal",
+          -- },
           args = { "--log-level", "DEBUG", "--quiet" },
           runner = "pytest",
+          python= ".env/bin/python",
+          pytest_discover_instances = false,
         })
       }
     })
